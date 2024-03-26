09bf36733ce1991d5da60fa614bcd87bed3c17e6
juliocesar2801 <149525435+juliocesar2801@users.noreply.github.com>
2024-03-26 18:09:58 -0300

    Update trabalho de 1atv bancos de dados.sql

--- trabalho de 1atv bancos de dados.sql
+++ trabalho de 1atv bancos de dados.sql
@@ -1,11 +1,23 @@
 create database sitemas_de_vendas_oline_22c;
 use	sitemas_de_vendas_oline_22c;
 
-create table (
-nome
-endereço
-email
+create table cliemte (
+nome varchar (150)not null,
+endereço varchar (170) not null,
+email varchar (130) not null,
+celular int(11) not null,
+dat_n  date not null 
+);
 
+create table produtos(
+nome varchar (150)not null,
+preço int not null,
+des text(65535) not null,
+quant varchar(50000)not null
+);
 
-
+create table pedido (
+np int not null,
+dt date not null,
+vlt bigint(18446744073709551615) not null
 );
